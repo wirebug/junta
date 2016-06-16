64ddcb021277b4fd758728b430ad2c77f1a5457f
Christoph Weinzierl <christoph.weinzierl@posteo.de>
2016-06-16 14:28:29 +0200

    add some more logic

--- GameServer/GameServer/App_Code/Konto.cs
+++ GameServer/GameServer/App_Code/Konto.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GameServer.App_Code.Karten;
 
 namespace GameServer.App_Code {
     public class Konto {
         Spieler spieler { get; set; }
-        int guthaben { get; set; }
+        public int guthaben { get; set; }
 
-        void addGuthaben(Karte karte)
+
+        /// <summary>
+        /// erhöht Guthaben mit der Summe von Geldkarte x
+        /// </summary>
+        /// <param name="karte">Muss eine Karte vom Typ Geldkarte sein</param>
+        public void increaseGuthaben(CreditsKarte karte)
         {
-            guthaben += karte.creditskarte;
+            guthaben += karte. ; //guthaben attribut fehlt noch
+        }
+
+        /// <summary>
+        /// veringert das Guthaben in Höhe des Parameters
+        /// </summary>
+        /// <param name="Summe">Credits die abgezogen werden sollen</param>
+        public void decreaseGuthaben(int Summe)
+        {
+            if (guthaben - Summe > 0)
+            {
+                guthaben -= Summe;
+            } else
+            {
+                throw ArgumentOutOfRangeException();
+            }
         }
     }
 }
