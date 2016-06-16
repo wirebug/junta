5c20c1f4e5fe0590908a3d030d85ee5e051e9534
DerMilchmann <jakob.rhb@googlemail.com>
2016-06-16 14:41:46 +0200

    Spieler conflict fix

--- GameServer/GameServer/App_Code/Spieler.cs
+++ GameServer/GameServer/App_Code/Spieler.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace GameServer.App_Code {
-    public class Spieler {
-       
+namespace GameServer.App_Code
+{
+    public class Spieler
+    {
+        public static int spielerCount = 0;
+        public int ID;
         public int Kampfmodifikator { get; set; }//Kampkarte InterplanetareGefechtsstations - bonus
+        public List<int> versprechungen;//Versprechungen sind noch nicht in der Hand und werden als ids übergeben
         public int flotten { get; set; }
         public int punkte { get; set; }
         public bool imperator { get; set; }
         public Planet planet { get; set; }
         public Hand hand { get; set; }
-
-        public Spieler(int flotten, int punkte,bool imperator, Planet planet, Hand hand)
+        public Spieler(int flotten, int punkte, bool imperator, Planet planet, Hand hand)
         {
             Kampfmodifikator = 0;
+        public Konto konto { get; set; }
+        public Kampf kampf { get; set; }
+
+        //Konstruktor NICHT FERTIG!!!, nur ID vergeben, rest fehlt
+        public Spieler()
+        {
+            spielerCount++;
+            ID = spielerCount;
         }
     }
 }
