76cf3adb20d20e27b4fc1803150bdc5e4b5c1f79
saph1r <sdonaq5@gmail.com>
2016-06-16 14:08:13 +0200

    Deck Stapel karten hinzugefügt
    alle IDs der Karten in Stapel hinzugefügt

--- GameServer/GameServer/App_Code/Karten/InterplanetareGefechtstationKarte.cs
+++ GameServer/GameServer/App_Code/Karten/InterplanetareGefechtstationKarte.cs
@@ -11,7 +11,7 @@ namespace GameServer.App_Code.Karten
      */
     public class InterplanetareGefechtstationKarte : Karte
     {
-        InterplanetareGefechtstationKarte(int value,Hand hand,Deck deck,Konto konto) : base(value,hand,deck,konto)
+        public InterplanetareGefechtstationKarte(int value,Hand hand,Deck deck,Konto konto) : base(value,hand,deck,konto)
         {
             
         }
